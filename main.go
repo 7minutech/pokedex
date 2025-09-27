c56039dea886244da0f92aedd69e98162d87a883
7minutech <elijohnson1107@gmail.com>
2025-09-27 13:22:07 -0500

    feat: basic repl that echos first word using cleanInput()

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if moreTokens := scanner.Scan(); !moreTokens {
+			log.Fatal(scanner.Err())
+		}
+		userInput := scanner.Text()
+		cleanInput := cleanInput(userInput)
+		output := fmt.Sprintf("Your command was: %v", cleanInput[0])
+		fmt.Println(output)
+	}
 }
 
 func cleanInput(text string) []string {
